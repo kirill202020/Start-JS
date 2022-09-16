9b47b3eb814b2c54bf0c26b8cdffe022309387ac
kirill202020 <91893674+kirill202020@users.noreply.github.com>
2022-09-16 18:30:26 +0200

    base rules

---
+++ script.js
@@ -0,0 +1,129 @@
+// lowerCamelStyle
+// CamelStyle 
+
+
+// console.log("Hello World!!")
+// console.log("Hello World!!");
+// console.warn("Hello World!!")
+// console.error("Hello World!!")
+
+// alert('HEllo world')
+
+// one line comment
+
+/* 
+many lines comment
+many lines comment
+many lines comment
+*/
+
+// console.log('hello world');//рядок
+// console.log(34123);//число
+// console.log(true);//логічний тип даних
+// console.log(undefined);//undefined
+// console.log(BigInt);//BigInt
+// console.log(Symbol);//Symbol
+// console.log(null);//null
+// console.log(Object);//object
+
+// = це присвоєння
+// +, -, *, / -- стандартні алгебраїчні оператори
+// ++, --   збільшити та зменшити на 1
+// >, <, =>, =<, ==, ===, !=  -- оператори порівняння
+
+// var a = '';
+// let b = 'hello';
+// const c = 'hello';
+
+// console.log(b);
+// b = 'hi'
+// console.log(b);
+
+// console.log(c);
+
+
+// let a = 4
+// let b = 5
+// let c = a+b
+// console.log(c); //=9
+
+// let a = '4'
+// let b = 5
+// let c = a+b
+// console.log(c); //=45
+
+
+// let a = '4'
+// let b = 5
+// let c = a - b
+// console.log(c); //=-1
+
+// let a = '4'
+// let b = 5
+// let c = Number(a) + b
+// console.log(c); //=9
+
+// let a = 4;
+// let b = 5;
+// console.log(b);//5
+// b--
+// console.log(b);//4
+
+
+// let a = 4
+// let b = 5
+// let c = a > b
+// console.log(c);
+
+
+
+// let a = false //false
+// let b = '' //false
+// let c = 0 //false
+// let d = undefined //false
+// let e = null //false
+
+
+// let a = true //true
+// let b = ' ' //true
+// let c = 3 //true
+
+// let a = 5
+// let b = 6
+
+// if (a < b) {
+//     alert(a)
+// }
+
+// let a = 5
+// let b = 4
+
+
+// if (a<b) {
+//     alert(a) 
+// } 
+// else {
+//     alert(b) 
+// }
+//виводиться найменше
+
+
+// let a = Math.floor(Math.random() *100)
+// let b = prompt()
+
+// if (b > a) {
+//     alert('too much')
+//     b = prompt()
+// }else if (b < a) {
+//     alert('small')
+// } else {
+    
+// }
+
+// let a = prompt('get a number', )
+// if (a > 0) {
+//     a++
+//     alert(a)
+// } else {
+//     alert(a)
+// }
